3cfed0d7a2d545aba8d9003f7bddf5e74cde7505
Mdparvez05 <mdparvezzz2000@gmail.com>
2026-01-31 23:29:15 +0530

    Update Patient.cs

--- backend/Models/Patient.cs
+++ backend/Models/Patient.cs
@@ -3,16 +3,30 @@
     public class Patient
     {
         public int Id { get; set; }
-        
+
         public string FirstName { get; set; }
+
         public string LastName { get; set; } = string.Empty;
+
         public string Email { get; set; }  = string.Empty;
+
         public string Phone {get; set; }  = string.Empty;
+
         public string Address { get; set; }  = string.Empty;
+
         public DateTime DateOfBirth { get; set; };
+
         public string CreatedBy { get; set; } ;
+
         public string UpdatedBy { get; set; }  = string.Empty;
+
         public DateTime CreatedOn { get; set; }; 
+
         public DateTime UpdatedOn { get; set;} = DateTime.MinValue;
+
+        public bool IsActive { get; set; } = true;
+
+
+
     }
 }
